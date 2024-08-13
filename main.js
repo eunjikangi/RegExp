0d95197e6784f94b18d3ea2c23ffa4ca85d61c62
eunjikangi <miki2323@naver.com>
2024-08-14 08:08:28 +0900

    05.패턴

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const str = `
 010-1234-4505.
 thesecon@naver.com
 https://www.naver.com
-The red cat jumps over the lazy dog.
+The frozen fox jumps over the lazy dog.
 abbcccdddd
+http://localhost
 `
 
 // const regexp = new RegExp('the', 'gi') // <- 생성자 방식
@@ -16,4 +17,26 @@ abbcccdddd
 // console.log(regexp.test(str))
 // console.log(str.replace(regexp, 'AAA'))
 
-console.log(str.match(/com$/gim))
+// console.log(
+//   str.match(/h..p/gm)
+// )
+
+// console.log(
+//     str.match(/https?/g) // '?'는 's'한정
+//   )
+
+// console.log(
+//       str.match(/\b\w{2,3}\b/g) // '?'는 's'한정
+//     )
+
+// console.log(
+//   str.match(/[a-c]{2}/g) // '?'는 's'한정
+// )
+
+// console.log(
+//   str.match(/\b\w{1,}/g) // '?'는 's'한정
+// )
+
+console.log(
+  str.match(/(?<=@).{1,}/g) // '?'는 's'한정
+)
